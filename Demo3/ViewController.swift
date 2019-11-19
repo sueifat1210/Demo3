087e8761fca729cbc0f362e317d5ec01e2d3faf0
wenwen <a0936291201@gmail.com>
2019-11-19 14:15:07 +0800

    change

--- Demo3/ViewController.swift
+++ Demo3/ViewController.swift
@@ -7,12 +7,45 @@
 //
 
 import UIKit
+import FacebookCore
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    func fetchImage(url: URL?, completionHandler: @escaping (UIImage?) -> ()) {
+        if let url = url {
+            let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+                if let data = data, let image = UIImage(data: data) {
+                    completionHandler(image)
+                } else {
+                    completionHandler(nil)
+                }
+            }
+            task.resume()
+        }
+    }
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        if let _ = AccessToken.current {
+            Profile.loadCurrentProfile { (profile, error) in
+                if let profile = profile {
+                    self.label.text = profile.name
+                    let url = profile.imageURL(forMode: .square, size: CGSize(width: 300, height: 300))
+                    
+                    self.fetchImage(url: url) { (image) in
+                        DispatchQueue.main.async {
+                            self.imageView.image = image
+
+                        }
+                    }
+                }
+            }
+        }
     }
 
 
